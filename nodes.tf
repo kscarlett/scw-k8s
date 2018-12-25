044ff7f4ef57dc3139ff960cee2402ff9c0f3311
kscarlett <kellen@kscarlett.com>
2018-12-26 00:04:36 +0100

    Reverse order of tags to show more relevant tags first

--- nodes.tf
+++ nodes.tf
@@ -3,7 +3,7 @@ resource "scaleway_server" "node" {
   name                = "${terraform.workspace}-node-${count.index + 1}"
   image               = "${data.scaleway_image.xenial.id}"
   type                = "${var.server_type}"
-  tags                = ["k8s", "k8s-worker", "${terraform.workspace}", "${terraform.workspace}-node"]
+  tags                = ["${terraform.workspace}-node", "${terraform.workspace}", "k8s-worker", "k8s"]
   dynamic_ip_required = true
   security_group      = "${scaleway_security_group.node_sec_group.id}"
 }

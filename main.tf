648deec4a279d883033e09eac73b0d138eca9623
kscarlett <kellen@kscarlett.com>
2018-12-25 16:59:19 +0100

    Changed image to be used

--- main.tf
+++ main.tf
@@ -4,8 +4,10 @@ provider "scaleway" {
 }
 
 resource "scaleway_server" "test_server" {
-  name                = "test_server"
-  image               = "e338d2ea-262d-45a1-95d2-300adce63cdd"
+  name = "test_server"
+
+  # Image: Ubuntu Xenial x86_64 in ams1
+  image               = "4035ca92-5292-4c6e-aa17-759fbc32765e"
   type                = "${var.server_type}"
   tags                = ["terraform", "test"]
   dynamic_ip_required = true

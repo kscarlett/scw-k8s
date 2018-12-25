0da5474d0aa5a4855749026ba0e4ab521bbe7aad
kscarlett <kellen@kscarlett.com>
2018-12-26 00:23:43 +0100

    Add IP reservation to nodes

--- nodes.tf
+++ nodes.tf
@@ -1,9 +1,13 @@
+resource "scaleway_ip" "node_ip" {
+  count = "${var.nodes}"
+}
+
 resource "scaleway_server" "node" {
-  count               = "${var.nodes}"
-  name                = "${terraform.workspace}-node-${count.index + 1}"
-  image               = "${data.scaleway_image.xenial.id}"
-  type                = "${var.server_type}"
-  tags                = ["${terraform.workspace}-node", "${terraform.workspace}", "k8s-worker", "k8s"]
-  dynamic_ip_required = true
-  security_group      = "${scaleway_security_group.node_sec_group.id}"
+  count          = "${var.nodes}"
+  name           = "${terraform.workspace}-node-${count.index + 1}"
+  image          = "${data.scaleway_image.xenial.id}"
+  type           = "${var.server_type}"
+  tags           = ["${terraform.workspace}-node", "${terraform.workspace}", "k8s-worker", "k8s"]
+  public_ip      = "${element(scaleway_ip.node_ip.*.ip, count.index)}"
+  security_group = "${scaleway_security_group.node_sec_group.id}"
 }
